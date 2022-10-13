36b5f4ba80951ca7481d4552225eb1033147de80
Nathan Boyd <nboyd@twilio.com>
2022-10-13 13:12:39 -0400

    cleanup kata 01

--- _scripts/cleanup.sh
+++ _scripts/cleanup.sh
@@ -9,7 +9,19 @@ APP_DIR="$(dirname "$SCRIPT_DIR")"
 
 logInfo "starting cleanup"
 
+################################################################################
+# cleanup kata_00
+
+# clean up rust kata
 rm -rf ./*/**/kata_00
+
+################################################################################
+# cleanup kata_0
+
+# clean up rust kata
 git checkout HEAD -- "${APP_DIR}/rust/01/kata_01/src/main.rs"
+rm "${APP_DIR}/rust/01/kata_01/Cargo.lock"
+
+################################################################################
 
 logInfo "done"
