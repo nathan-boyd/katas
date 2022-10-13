089d983054eb73db755f45c7f4dfc6982fc9afb7
Nathan Boyd <nboyd@twilio.com>
2022-10-13 12:56:44 -0400

    remove kata_ignore

--- _scripts/cleanup.sh
+++ _scripts/cleanup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+set -e
+
 source "${HOME}/.zshfn/logInfo"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+APP_DIR="$(dirname "$SCRIPT_DIR")"
 
 logInfo "starting cleanup"
 
-rm -rf ./*/**/kata_**
+rm -rf ./*/**/kata_00
+git checkout HEAD -- "${APP_DIR}/rust/01/kata_01/src/main.rs"
 
 logInfo "done"
