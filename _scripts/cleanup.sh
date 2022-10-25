3f73cb786be2b8714ac837e656f85a34a2d9898b
Nathan Boyd <nboyd@twilio.com>
2022-10-25 10:04:02 -0400

    cleanup golang kata

--- _scripts/cleanup.sh
+++ _scripts/cleanup.sh
@@ -2,12 +2,13 @@
 
 set -e
 
+# shellcheck disable=SC1091
 source "${HOME}/.zshfn/logInfo"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 APP_DIR="$(dirname "$SCRIPT_DIR")"
 
-logInfo "starting cleanup"
+logInfo "starting cleanup in ${APP_DIR}"
 
 ################################################################################
 # cleanup kata_00
@@ -16,11 +17,15 @@ logInfo "starting cleanup"
 rm -rf ./*/**/kata_00
 
 ################################################################################
-# cleanup kata_0
+# cleanup kata_01
 
 # clean up rust kata
 git checkout HEAD -- "${APP_DIR}/rust/01/kata_01/src/main.rs"
 rm -rf "${APP_DIR}/rust/01/kata_01/Cargo.lock"
+
+# clean up go kata
+rm -rf "${APP_DIR}"/golang/01/*.mod
+rm -rf "${APP_DIR}"/golang/01/*.go
 
 ################################################################################
 # reset submodules, like vim-kata
